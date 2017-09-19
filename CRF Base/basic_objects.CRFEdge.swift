76f3ddb2a37b16e6f669fc5fdf62a715f3a5b7e8
Andre Städtler <andre.staedtler93@gmail.com>
2017-09-19 15:33:33 +0200

    removed useless file

--- CRF Base/basic_objects.CRFEdge.swift
+++
@@ -1,9 +0,0 @@
-//
-//  basic_objects.CRFEdge.swift
-//  CRF Base
-//
-//  Created by Andre Staedtler on 19.09.17.
-//  Copyright © 2017 Andre Staedtler. All rights reserved.
-//
-
-import Foundation

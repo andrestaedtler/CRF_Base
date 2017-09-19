382e1f6638641e399a685194073f3940af30d1a2
Andre Städtler <andre.staedtler93@gmail.com>
2017-09-19 09:58:18 +0200

    Initial Commit

---
+++ CRF Base/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  CRF Base
+//
+//  Created by Andre Staedtler on 19.09.17.
+//  Copyright © 2017 Andre Staedtler. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
